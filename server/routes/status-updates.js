9bf1aae10d5cfeae588c4c43115367148525dcd5
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-11-18 14:13:35 -0600

    defined the update status function

--- server/routes/status-updates.js
+++ server/routes/status-updates.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-import { getAllUpdates } from "../controllers/status-update.controller";
+import { getAllUpdates } from "../controllers";
 
-router.get('/', getAllUpdates)
+router.get('/', getAllUpdates);
+router.put('/update', updateStatus);
 module.exports = router;
