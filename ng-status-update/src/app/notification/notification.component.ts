415c92580f11b660d1880646ea101cb11a916e20
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-11-17 22:32:39 -0600

    notification component, currently default

---
+++ ng-status-update/src/app/notification/notification.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-notification',
+  templateUrl: './notification.component.html',
+  styleUrls: ['./notification.component.scss']
+})
+export class NotificationComponent {
+
+}
