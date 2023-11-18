b0dbb4cb30ef909a1d2404544a5a4ac65214e1f4
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-11-17 21:54:32 -0600

    imported the status-update, notification components

--- ng-status-update/src/app/app.module.ts
+++ ng-status-update/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StatusUpdateComponent } from './status-update/status-update.component';
+import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatusUpdateComponent,
+    NotificationComponent
   ],
   imports: [
     BrowserModule,
