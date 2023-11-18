ac9881b84c84ea9878a888a92eae85ba09d78f65
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-11-18 14:01:19 -0600

    defined the status update route

--- ng-status-update/src/app/app-routing.module.ts
+++ ng-status-update/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { StatusUpdateComponent } from './status-update/status-update.component';
+const routes: Routes = [
+    {
+        path: '',
+        component: StatusUpdateComponent,
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
