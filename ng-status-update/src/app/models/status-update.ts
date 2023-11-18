a0cd6663c152456bcd6c5919fc6f7c38e9e6709c
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-11-18 13:40:00 -0600

    defined the interface for the status update component

--- ng-status-update/src/app/models/status-update.ts
+++ ng-status-update/src/app/models/status-update.ts
@@ -1,2 +1,12 @@
+import { Reply } from "./reply";
 export interface StatusUpdate {
-}
+    id: number;
+    user_id: number;
+    status_text: string;
+    created_at: Date;
+    updated_at?: Date;
+    likes: number;
+    replies: Reply[];
+  }
+  
+  
